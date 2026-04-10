0d85f3449301b6a8385707a92d1da699f9bff4f6
Sahasra-DevSecops <uicloudops@gmail.com>
2026-04-10 22:35:53 +0530

    roboshop project

--- roboshop.sh
+++ roboshop.sh
@@ -2,7 +2,8 @@
 
 SG_ID="sg-000c5bbdae937d72f"
 AMI_ID="ami-0220d79f3f480ecf5"
-
+ZONE_ID="Z09891872RAM18EYN37Z5"
+DOMAIN_NAME="daws88s.online"
 for instance in $@
 do
     # Fix 1: Change instance_id to InstanceId (Case Sensitive)
@@ -22,14 +23,41 @@ do
         IP=$(aws ec2 describe-instances \
             --instance-ids $instance_id \
             --query 'Reservations[0].Instances[0].PublicIpAddress' \
-            --output text)
+            --output text
+			)
+						RECORD_NAME="$DOMAIN_NAME" # daws88s.online
+
     else
         # Fix 3: Add Reservations[0] to the query path
         IP=$(aws ec2 describe-instances \
             --instance-ids $instance_id \
             --query 'Reservations[0].Instances[0].PrivateIpAddress' \
-            --output text)
+            --output text
+			)
+			RECORD_NAME="$instance.$DOMAIN_NAME" # mongodb.daws88s.online
+
     fi
 
     echo "$instance IP Address: $IP"
+
+	aws route53 change-resource-record-sets \
+  --hosted-zone-id $ZONE_ID \
+  --change-batch '{
+  "comment": "Update a record",
+    "Changes": [
+      {
+        "Action": "UPSERT",
+        "ResourceRecordSet": {
+          "Name": "'$RECORD_NAME'",
+          "Type": "A",
+          "TTL": 1,
+          "ResourceRecords": [
+            { "Value": "'$IP'" }
+          ]
+        }
+      }
+    ]
+  }'
+
+echo "record updated for $instance"
 done
